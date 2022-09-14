cee39e04ed6d3ee8c1aca49c01648ceead2030bb
Alexander <alexandrov.alexandder@gmail.com>
2022-09-14 08:54:29 +0300

    Made GUI

---
+++ SerialPorts/COM_Ports/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,54 @@
+﻿using COM_Ports.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COM_Ports.MVVM.ViewModel
+{
+    internal class MainViewModel : ObservableObject
+    {
+        private string _sendMessage;
+
+        public string sendMessage
+        {
+            get { return _sendMessage; }
+            set
+            {
+                _sendMessage = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _receivedMessage;
+
+        public string receivedMessage
+        {
+            get { return _receivedMessage; }
+            set
+            {
+                _receivedMessage = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private StringBuilder _logs = new StringBuilder();
+
+        public StringBuilder logs {
+            get { return _logs; }
+            set
+            {
+                _logs = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+
+        }
+
+
+    }
+}
